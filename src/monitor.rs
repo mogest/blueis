e418d0fd30ec1f1a345759cc80f6f0474f20eb63
Roger Nesbitt <roger@seriousorange.com>
2017-07-22 13:38:32 +1200

    Add monitor tests

--- src/monitor.rs
+++ src/monitor.rs
@@ -71,3 +71,63 @@ impl<'a> Listener<'a> {
         Some(payload.unwrap().clone())
     }
 }
+
+#[cfg(test)]
+mod tests {
+    use super::Monitor;
+    use std::{thread, time};
+
+    #[test]
+    fn monitor_acts_as_an_mpmc_queue() {
+        let monitor = Monitor::new();
+
+        monitor.send("will never be received".to_string());
+
+        let listener_a = monitor.listen();
+        monitor.send("A".to_string());
+
+        let listener_b = monitor.listen();
+        monitor.send("B".to_string());
+
+        assert_eq!(listener_a.recv().unwrap(), "A".to_string());
+        assert_eq!(listener_a.recv().unwrap(), "B".to_string());
+        assert_eq!(listener_b.recv().unwrap(), "B".to_string());
+    }
+
+    #[test]
+    fn monitor_can_be_shared_with_threads() {
+        let monitor = Monitor::new();
+        let mut receivers = vec![];
+
+        for _ in 0..4 {
+            let local_monitor = monitor.clone();
+
+            receivers.push(thread::spawn(move || {
+                let listener = local_monitor.listen();
+                let mut payloads = (0..4).map(|_| listener.recv().unwrap()).collect::<Vec<String>>();
+                payloads.sort();
+                assert_eq!(payloads, vec!["0".to_string(), "1".to_string(), "2".to_string(), "3".to_string()]);
+            }));
+        }
+
+        thread::sleep(time::Duration::from_millis(100));
+
+        for index in 0..4 {
+            let local_monitor = monitor.clone();
+
+            thread::spawn(move || {
+                local_monitor.send(index.to_string());
+            });
+        }
+
+        for receiver in receivers {
+            receiver.join().unwrap();
+        }
+    }
+
+    #[test]
+    fn nothing_bad_happens_when_you_send_with_no_listeners() {
+        let monitor = Monitor::new();
+        monitor.send("some payload".to_string());
+    }
+}
